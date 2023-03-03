cfe4be91e882014087779f0ed648fca81954704c
Alberto Secci <a.secci@stamtech.com>
2023-03-03 01:13:43 +0100

    hybrid solution for bg change animation

--- src/app/components/call.component.ts
+++ src/app/components/call.component.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {CallerComponent} from './caller.component';
 import {PreCallActionsComponent} from './actions/pre-call-actions.component';
 import {CallActionsComponent} from './actions/call-actions.component';
 import {HeaderComponent} from './header.component';
 import {CallService} from '../services/call.service';
-import {CommonModule} from '@angular/common';
 
 @Component({
     standalone: true,
@@ -12,7 +12,7 @@ import {CommonModule} from '@angular/common';
     imports: [CommonModule, HeaderComponent, CallerComponent, PreCallActionsComponent, CallActionsComponent],
     providers: [CallService],
     template: `
-        <div class="relative h-full overflow-hidden">
+        <div class="relative h-full sm:rounded-3xl overflow-hidden">
             <div class="flex flex-col justify-between items-center py-16 h-full w-full">
                 <div class="flex flex-col gap-10 w-full">
                     <app-header/>
@@ -24,7 +24,8 @@ import {CommonModule} from '@angular/common';
                     <app-call-actions *ngIf="(cs.status | async) === 'ACCEPTED'"/>
                 </div>
             </div>
-            <div class="absolute left-16 bottom-16 transition-all duration-1000 rounded-full w-8 h-8 max-w-full max-h-screen bg-gradient-to-t from-green-200 via-green-300 to-blue-500 -z-10 opacity-0"
+            <div class="absolute left-16 bottom-16 transition-all duration-1000 rounded-full h-[5%] aspect-square 
+            bg-gradient-to-t from-green-200 via-green-300 to-blue-500 -z-10 opacity-0"
                  [ngClass]="(cs.status | async) === 'ACCEPTED' ? 'after' : null">
             </div>
         </div>
@@ -36,7 +37,7 @@ import {CommonModule} from '@angular/common';
       };
 
       .after {
-        transform: scale(100);
+        transform: scale(calc(100 / 5) * 2);
         opacity: 100%;
       };
     `]
--- src/app/layout.component.ts
+++ src/app/layout.component.ts
@@ -7,7 +7,7 @@ import {RouterModule} from '@angular/router';
     imports: [RouterModule],
     template: `
         <div class="flex justify-center items-center h-screen w-full bg-gradient-to-t from-purple-500 to-pink-500">
-            <div class="flex justify-center items-center w-[390px] h-screen sm:h-[80%] sm:rounded-3xl backdrop-blur-lg bg-white/30 ">
+            <div class="flex justify-center items-center w-full sm:w-[390px] h-screen sm:h-[80%] sm:rounded-3xl backdrop-blur-lg bg-white/30 ">
                 <router-outlet></router-outlet>
             </div>
         </div>
